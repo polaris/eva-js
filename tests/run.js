157b491f4d61036ed87fb4342a80b9e03e162fd3
Jan Deinhard <jan.deinhard@gmail.com>
2024-01-07 22:12:35 +0100

    chore: re-order tests

--- tests/run.js
+++ tests/run.js
@@ -2,19 +2,19 @@ const Eva = require("../Eva");
 const Environment = require("../Environment");
 
 const tests = [
-  require("./built-in-function-test"),
-  require("./user-defined-function-test"),
-  require("./self-eval-test"),
-  require("./math-test"),
-  require("./comparison-test"),
-  require("./inc-test"),
-  require("./dec-test"),
-  require("./logical-ops-test"),
-  require("./variables-test"),
   require("./block-test"),
+  require("./built-in-function-test"),
+  require("./comparison-test"),
+  require("./dec-test"),
   require("./if-test"),
-  require("./while-test"),
+  require("./inc-test"),
   require("./lambda-function-test"),
+  require("./logical-ops-test"),
+  require("./math-test"),
+  require("./self-eval-test"),
+  require("./user-defined-function-test"),
+  require("./variables-test"),
+  require("./while-test"),
 ];
 
 const eva = new Eva();
